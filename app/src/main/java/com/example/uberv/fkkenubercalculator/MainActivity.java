e687ca5331dbeaba764afafaf429bcc1ec565321
ricard.arman.kuodis <ricard.arman.kuodis@accenture.com>
2017-02-02 18:06:53 +0200

    update

--- app/src/main/java/com/example/uberv/fkkenubercalculator/MainActivity.java
+++ app/src/main/java/com/example/uberv/fkkenubercalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.uberv.fkkenubercalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,21 +22,33 @@ import net.sourceforge.jeval.Evaluator;
 public class MainActivity extends AppCompatActivity {
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    public static final char NULL_CHAR = '\u0000';
+
     @BindView(R.id.input_tv)
     TextView mInputTv;
     @BindView(R.id.output_tv)
     TextView mOutputTv;
-    private String mEquation = "";
+    @BindView(R.id.op_button_del)
+    Button mDeleteBtn;
+    private StringBuilder mEquationBuilder = new StringBuilder();
+
     private Evaluator mEvaluator;
+    private char lastChar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
 
         mEvaluator = new Evaluator();
+        mDeleteBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                delete();
+                return true;
+            }
+        });
     }
 
     @OnClick({R.id.digit_btn_0, R.id.digit_btn_1, R.id.digit_btn_2,
@@ -45,40 +58,79 @@ public class MainActivity extends AppCompatActivity {
             R.id.op_button_add, R.id.op_button_sub,
             R.id.op_button_div})
     void OnDigitInput(Button button) {
-        mEquation = mInputTv.getText().toString() + button.getText();
-        mInputTv.setText(mEquation);
-        mEquation = mEquation.replace('÷', '/');
+        char newChar = button.getText().toString().charAt(0);
+        // if last char was operation, and new char is operation, then replace last operation
+        if (isOperation(lastChar) && isOperation(newChar)) {
+            mEquationBuilder.deleteCharAt(mEquationBuilder.length() - 1).append(newChar);
+        } else {
+            mEquationBuilder.append(newChar);
+        }
+        lastChar = newChar;
+        mInputTv.setText(mEquationBuilder.toString());
         calculate();
     }
 
     private void calculate() {
         // operate on mEquation
         String output = new String();
-        try {
-            output = mEvaluator.evaluate(mEquation);
-            Log.d(LOG_TAG, "calculating: " + mEquation + " = " + output);
-        } catch (EvaluationException e) {
-            Log.w(LOG_TAG, "failed to calculate " + mEquation);
+        if (mEquationBuilder.length() > 0) {
+            String tmpEquation = isOperation(mEquationBuilder.charAt(mEquationBuilder.length() - 1)) ?
+                    mEquationBuilder.toString().substring(0, mEquationBuilder.length() - 1) :
+                    mEquationBuilder.toString();
+            tmpEquation = tmpEquation.replace('÷', '/');
+            try {
+                output = mEvaluator.evaluate(tmpEquation);
+                Log.d(LOG_TAG, "calculating: " + tmpEquation + " = " + output);
+            } catch (EvaluationException e) {
+                Log.w(LOG_TAG, "failed to calculate " + tmpEquation);
 //            e.printStackTrace();
-        }
-        if(!isEmptyOrNull(output)){
-            mOutputTv.setText(output);
+            }
+            if (!isEmptyOrNull(output)) {
+                mOutputTv.setText(output);
+            }
+        }else{
+            // nothing to calculate (reset text incase of backspace function)
+            mOutputTv.setText("");
         }
     }
 
-    @OnClick(R.id.op_button_del)
+
     void delete() {
-        mEquation = "";
+        mEquationBuilder = new StringBuilder();
         mOutputTv.setText("");
         mInputTv.setText("");
     }
 
+    @OnClick(R.id.op_button_del)
+    void backspace() {
+        if (mEquationBuilder.length() > 0) {
+            // backspace
+            mEquationBuilder.deleteCharAt(mEquationBuilder.length() - 1);
+            // check if we have characters left
+            if (mEquationBuilder.length() > 0) {
+                // since we delete last character, set lastChar to new last character
+                lastChar = mEquationBuilder.charAt(mEquationBuilder.length() - 1);
+            } else {
+                // nothing left...
+                lastChar = NULL_CHAR;
+            }
+            // since we delete last character, set lastChar to new last character
+            mInputTv.setText(mEquationBuilder.toString());
+            // recalculate answer
+            calculate();
+        }
+    }
+
     @OnClick(R.id.op_button_equals)
-    void equals(){
+    void equals() {
 
     }
 
     private boolean isEmptyOrNull(String string) {
         return string == null || string.isEmpty();
+    }
+
+    private boolean isOperation(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '÷';
     }
 }
